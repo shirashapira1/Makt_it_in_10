9d8a84bcf1fc19e8342d6b77b8c744d39dc17627
shirashapira1 <137098074+shirashapira1@users.noreply.github.com>
2023-06-25 12:33:31 +0300

    intial commit

---
+++ jScripts/JavaScript.js
@@ -0,0 +1,30 @@
+﻿var imageIndex = 1;
+showImages(imageIndex);
+
+
+function plusSlides(n) {
+    showImages(imageIndex += n);
+}
+
+
+function currentSlide(n) {
+    showImages(imageIndex = n);
+}
+
+function showImages(n) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("demo");
+    var captionText = document.getElementById("caption");
+    if (n > slides.length) { imageIndex = 1 }
+    if (n < 1) { imageIndex = slides.length }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active","");
+    }
+    slides[imageIndex - 1].style.display = "block";
+    dots[imageIndex - 1].className += " active";
+    captionText.innerHTML = dots[imageIndex - 1].alt;
+}
